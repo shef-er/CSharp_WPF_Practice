b2129b792ff95b06c41529393fb174d8dfdde382
Ernest Shefer <shef-er@ya.ru>
2018-02-20 10:35:59 +0500

    logics done

--- Practice01/Program.cs
+++ Practice01/Program.cs
@@ -1,51 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Mime;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CSharp_WPF_Practice
 {
-    struct Position
-    {
-        private double x, y;
 
-        public Position(double x = 0, double y = 0)
+    class Simulation
+    {
+        private double delta_t;
+        private Body body;
+        private bool running;
+        private double time;
+
+        public Simulation(Body body, double delta_t = 0.001)
         {
-            this.x = x;
-            this.y = y;
+            this.delta_t = delta_t;
+            this.body = body;
+            this.time = 0;
+            this.running = true;
+        }
+
+        public void Run()
+        {
+            while (this.running)
+            {
+                time += delta_t;
+                running = this.Step();
+            }
+        }
+
+        private bool Step()
+        {
+            var position = body.Move(this.time);
+            Console.WriteLine($"Time: {time}\nPosition: ({position[0]}, {position[1]})");
+            
+            return !(position[1] <= 0);
         }
     }
 
-    
     class Body
     {
-        const double acceleration = 9.80665;
+        private const double acceleration = 9.80665;
         
+        private double angle;
+        private double velocity;
         private double mass;
-        private double v0, v1;
-        private Position position;
+        private double x, y;
         
-        public Body(double mass = 1, double x = 0, double y = 0)
+        public Body(double angle, double velocity,
+                    double mass = 1, double x = 0, double y = 0)
         {
+            this.angle = angle;
+            this.velocity = velocity;
             this.mass = mass;
-            this.position = new Position(x, y);
-            Console.WriteLine($"\nMass: {mass}\nVelocity: {velocity}");
+            this.x = x;
+            this.y = y;
             
+            Console.WriteLine($"\nNew Body:\nAngle: {angle}\nv0: {velocity}\nm: {mass}"); 
         }
-        
-        public void Throw(double angle, double start_velocity)
-        {
-            this.v0 = start_velocity;
 
+        public double[] Move(double t)
+        {
+            double x, y;
+            x = this.x + velocity * t * Math.Cos(angle);
+            y = this.y + velocity * t * Math.Sin(angle) - mass * acceleration * t * t / 2;
+            
+            return new[] {x, y};
         }
+
+        public double[] Position => new[] { x, y };
     }
-    
     
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {   
-            Body ball = new Body();
-            
-            
+            Body ball = new Body(Math.PI/4, 10);
+            Simulation flight = new Simulation(ball);
+            flight.Run();
         }
     }
 }
