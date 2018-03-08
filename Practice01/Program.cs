289155d614791d158ef0895c27596439c88d5f1b
Ernest Shefer <shef-er@ya.ru>
2018-03-09 01:42:21 +0500

    drawing and file i/o

--- Practice01/Program.cs
+++ Practice01/Program.cs
@@ -1,14 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Windows;
+using System.Windows.Forms;
 
-namespace CSharp_WPF_Practice
+namespace Practice01
 {
-    class Program
+    class Program : Form
     {
-        private static void Main(string[] args)
-        {   
-            Body ball = new Body(Math.PI/4, 10);
-            Simulation flight = new Simulation(ball);
+        private Dictionary<string, double> parameters;
+
+        private Program(Dictionary<string, double> parameters)
+        {
+            this.Text = "Body flight simulation";
+            this.Size = new Size(1600, 1080);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            this.parameters = parameters;
+        }
+         
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics formGraphics = this.CreateGraphics();
+
+            Body ball = new Body(parameters["angle"], parameters["v0"],
+                parameters["mass"], parameters["x0"], parameters["y0"]);
+
+            Simulation flight = new Simulation(ball, formGraphics);
             flight.Run();
+    
+        }
+        
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            Dictionary<string, double> parameters = new Dictionary<string, double>();
+            parameters.Add("angle", 0);
+            parameters.Add("v0", 0);
+            parameters.Add("mass", 1);
+            parameters.Add("x0", 0);
+            parameters.Add("y0", 0);
+
+            try
+            {
+                using (var sr = new StreamReader("input.txt"))
+                {
+                    String line;
+                    while ( (line = sr.ReadLine()) != null )
+                    {
+                        var items = line.Split(':');
+                        if (items[1] != null)
+                        {
+                           var key = items[0].Trim();
+                           var value = Convert.ToDouble(items[1].Trim(), CultureInfo.InvariantCulture);
+                           parameters[key] = value;
+                        }
+                    }
+                    sr.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            
+            Application.Run(new Program(parameters));
         }
     }
 }
--- Practice01/Simulation.cs
+++ Practice01/Simulation.cs
@@ -1,48 +1,78 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
 
-namespace CSharp_WPF_Practice
+namespace Practice01
 {
     public class Simulation
     {
-        private double delta_t;
+        private const int sceneHeight = 900;
+        
         private Body body;
-        private bool running;
+        private Graphics graphics;
+        private double delta_t;
         private double time;
+        private bool running;
 
-        public Simulation(Body body, double delta_t = 0.01)
+        public Simulation(Body body, Graphics graphics)
         {
-            this.delta_t = delta_t;
+            this.delta_t = 0.01;
             this.body = body;
             this.time = 0;
             this.running = true;
+            this.graphics = graphics;
         }
 
         public void Run()
         {
+            DrawScene();
+            DrawBody();
+            
             while (this.running)
             {
                 time += delta_t;
                 running = this.Step();
+                
+                DrawBody();
+            }
+            
+            using (StreamWriter sw = new StreamWriter("output.txt")) 
+            {
+                var position = this.body.Position;
+
+                sw.WriteLine("\nTime: {0}\nPosition: ({1}, {2})\n",
+                    time, position[0], position[1]);
+                sw.Close();
             }
         }
 
         private bool Step()
         {
-            var position = body.Move(this.time);
+            var position = this.body.Move(this.time);
             
-            if (position[1] <= 0)
-            {
-                Console.WriteLine($"Time: {time}\nPosition: ({body.Length}, 0)\n");
-                return false;
-            }
-            
-            Console.WriteLine($"Time: {time}\nPosition: ({position[0]}, {position[1]})\n");
-            return true;
+            //Console.WriteLine("\nTime: {0}\nPosition: ({1}, {2})\n", time, position[0], position[1]);
+
+            return !(position[0] > 0 && position[1] == 0) &&
+                   !(position[0] == 0 && position[1] < 0);
+        }
+
+        private void DrawScene()
+        {   
+            graphics.FillRectangle(new SolidBrush(Color.DeepSkyBlue), 0,   0, 10000, 10000);
+            graphics.FillRectangle(new SolidBrush(Color.ForestGreen), 0, sceneHeight, 10000, 10000);
+        }
+
+        private void DrawBody()
+        {
+            var position = this.body.Position;
+
+            graphics.FillEllipse(new SolidBrush(Color.DeepPink),
+                (int)(position[0]*2), sceneHeight -( (int)(position[1]*2) +2), 2, 2);
         }
         
         public ulong GetCurrentTime()
         {
-            TimeSpan time = (DateTime.UtcNow - new DateTime(1970, 1, 1));
+            TimeSpan time = DateTime.UtcNow - new DateTime(1970, 1, 1);
             return (ulong) time.TotalSeconds;
         }
     }
@@ -52,34 +82,43 @@ namespace CSharp_WPF_Practice
         private const double gravity = 9.80665;
         
         private double angle;
-        private double velocity;
+        private double v0;
         private double mass;
         private double x0, y0, x, y, length;
         
-        public Body(double angle, double velocity,
+        public Body(double angle, double v0,
                     double mass = 1, double x0 = 0, double y0 = 0)
         {
             this.angle = angle;
-            this.velocity = velocity;
+            this.v0 = v0;
             this.mass = mass;
             this.x0 = x0;
             this.y0 = y0;
 
-            this.length = velocity * velocity * Math.Sin(2 * angle) / gravity;
+            this.length = v0 * v0 * Math.Sin(2 * angle) / gravity;
             
-            Console.WriteLine($"\nNew Body:\nAngle: {angle}\nv0: {velocity}\nm: {mass}"); 
+            Console.WriteLine(
+                "\nNew Body:\n  Angle: {0}\n  Velocity: {1}\n  Mass: {2}\n  Position: ({3}, {4})\n",
+                angle, v0, mass, x0, y0
+            );
         }
 
         public double[] Move(double t)
         {
-            x = x0 + velocity * t * Math.Cos(angle);
-            y = y0 + velocity * t * Math.Sin(angle) - mass * gravity * t * t / 2;
+            x = x0 + v0 * t * Math.Cos(angle);
+            y = y0 + v0 * t * Math.Sin(angle) - mass * gravity * t * t / 2;
+            
+            if (x > 0 && y <= 0)
+            {
+                x = this.length;
+                y = 0;
+            }
             
             return this.Position;
         }
         
-        public double Length => length;
-
-        public double[] Position => new[] { x, y };
+        public double[] Position {    
+            get { return new[] { x, y }; }
+        }
     }
 }
