a31a1fbc1a5d0504a4332a6a5d0f25cc48748e3a
Ernest Shefer <shef-er@ya.ru>
2018-04-27 01:37:33 +0500

    some fixes

---
+++ Practice01/Body.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace Practice01
+{
+    public class Body
+    {
+        private const double gravity = 9.80665;
+        
+        private double angle;
+        private double v0;
+        private double mass;
+        private double x0, y0, x, y, length;
+        
+        public Body(double angle, double v0,
+            double mass = 1, double x0 = 0, double y0 = 0)
+        {
+            this.angle = angle;
+            this.v0 = v0;
+            this.mass = mass;
+            this.x0 = x0;
+            this.y0 = y0;
+
+            this.length = v0 * v0 * Math.Sin(2 * angle) / gravity;
+            
+            Console.WriteLine(
+                "\nNew Body:\n  Angle: {0}\n  Velocity: {1}\n  Mass: {2}\n  Position: ({3}, {4})\n",
+                angle, v0, mass, x0, y0
+            );
+        }
+
+        public double[] Move(double t)
+        {
+            x = x0 + v0 * t * Math.Cos(angle);
+            y = y0 + v0 * t * Math.Sin(angle) - mass * gravity * t * t / 2;
+            
+            if (x > 0 && y <= 0)
+            {
+                x = this.length;
+                y = 0;
+            }
+            
+            return this.Position;
+        }
+        
+        public double[] Position {    
+            get { return new[] { x, y }; }
+        }
+    }
+}
--- Practice01/Program.cs
+++ Practice01/Program.cs
@@ -4,165 +4,292 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Globalization;
 using System.IO;
-using System.Windows;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Practice01
 {
-    class Program : Form
-    {
-        private String[] inputTags = {"angle", "v0", "mass", "x0", "y0"};
-        private String[] inputLabels = { "Angle", "Velocity", "Mass", "X", "Y" };
-        private Dictionary<string, double> parameters;
-        private Control fieldResult;
-        private Control[] inputArray;
-        private Control[] labelArray;
-        private Graphics formGraphics;
-        private Point[] arrow = new Point[2];
+	class Program : Form
+	{
+		private CultureInfo invC = CultureInfo.InvariantCulture;
+		
+		private Dictionary<string, double> parameters;
+		private Dictionary<string, string> labels;
+		
+		private Control fieldResult;
+		private Point[] arrow = new Point[2];
+		
+		private Graphics formGraphics;
+		
+		private Program(string filename)
+		{
+			this.Text = "Body flight simulation";
+			this.Size = new Size(800, 600);
+			this.StartPosition = FormStartPosition.CenterScreen;
+			
+			parameters = new Dictionary<string, double>();
+			parameters.Add("angle", 0);
+			parameters.Add("v0", 0);
+			parameters.Add("mass", 1);
+			parameters.Add("x0", 0);
+			parameters.Add("y0", 0);
 
-        private Program(Dictionary<string, double> parameters)
-        {
-            this.parameters = parameters;
-            
-            this.Text = "Body flight simulation";
-            this.Size = new Size(800, 600);
-            this.StartPosition = FormStartPosition.CenterScreen;
+			this.ReadParametersFromFile(filename);
+			
+			labels = new Dictionary<string, string>();
+			labels.Add("angle", "Angle:");
+			labels.Add("v0", "Velocity:");
+			labels.Add("mass", "Mass:");
+			labels.Add("x0", "Start X:");
+			labels.Add("y0", "Start Y:");
+			
+			// Controls
+			int index = 0;
+			foreach(var item in parameters)
+			{
+				Control input = new TextBox();
+				input.Location = new Point(30 + index*100, 30);
+				input.Size = new Size(60, 20);
+				input.Name = item.Key;
+				input.Text = item.Value.ToString(invC);
+				
+				Control label = new Label();
+				label.Location = new Point(30 + index*100, 10);
+				label.Size = new Size(60, 20);
+				label.Text = labels[item.Key];
+				
+				this.Controls.Add(input);
+				this.Controls.Add(label);
+				index++;
+			}
 
-            formGraphics = this.CreateGraphics();
+			Button buttonStart = new Button();
+			buttonStart.Location = new Point(30, 70);
+			buttonStart.Size = new Size(60, 25);
+			buttonStart.Text = "Start";
+			buttonStart.Click += buttonStart_Click;
 
-            // Controls
-            inputArray = new Control[5];
-            labelArray = new Control[5];
+			fieldResult = new TextBox();
+			fieldResult.Location = new Point(130, 70);
+			fieldResult.Size = new Size(360, 20);
 
-            for (var i = 0; i < 5; i++ ) 
-            {
-                inputArray[i] = new TextBox();
-                inputArray[i].Location = new Point(30 + i*100, 30);
-                inputArray[i].Size = new Size(60, 20);
-                inputArray[i].Text = this.parameters[inputTags[i]].ToString(CultureInfo.InvariantCulture);
-                
-                labelArray[i] = new Label();
-                labelArray[i].Location = new Point(30 + i*100, 10);
-                labelArray[i].Size = new Size(60, 20);
-                labelArray[i].Text = inputLabels[i];
-                
-                this.Controls.Add(inputArray[i]);
-                this.Controls.Add(labelArray[i]);
-                
-            }
-            
-            Button buttonStart = new Button();
-            buttonStart.Location = new Point(30, 70);
-            buttonStart.Size = new Size(60, 25);
-            buttonStart.Text = "Start";
-            buttonStart.Click += buttonStart_Click;
-            
-            fieldResult = new TextBox();
-            fieldResult.Location = new Point(130, 70);
-            fieldResult.Size = new Size(360, 20);
-            
-            this.Controls.Add(buttonStart);
-            this.Controls.Add(fieldResult);
+			this.Controls.Add(buttonStart);
+			this.Controls.Add(fieldResult);
 
-            this.MouseDown += form_OnMouseDown;
-            this.MouseUp += form_OnMouseUp;
+			this.MouseDown += form_OnMouseDown;
+			this.MouseUp += form_OnMouseUp;
+			
+			// Create graphics object
+			formGraphics = this.CreateGraphics();
+		}
 
-        }
+		// Deprecated constructor
+		private Program(Dictionary<string, double> parameters, 
+						Dictionary<string, string> labels)
+		{
+			this.parameters = parameters;
+			this.labels = labels;
 
-        private void DrawSimulation()
-        {
-            Body ball = new Body(parameters["angle"], parameters["v0"],
-                parameters["mass"], parameters["x0"], parameters["y0"]);
-            Simulation flight = new Simulation(ball, formGraphics);
-            var result = flight.Run();
-            
-            fieldResult.Text = String.Format("Result: ({1}, {2}) after {0} seconds",
-                result[2], result[0], result[1]);
-        }
+			this.Text = "Body flight simulation";
+			this.Size = new Size(800, 600);
+			this.StartPosition = FormStartPosition.CenterScreen;
 
-        private void ChangeParameterByName(String name, String value)
-        {
-            this.parameters[name] = Convert.ToDouble(value, CultureInfo.InvariantCulture);
-        }
+			formGraphics = this.CreateGraphics();
+			
+			/*// Controls (Deprecated)
+			private Control[] inputArray = new Control[5];
+			private Control[] labelArray = new Control[5];
 
-        private void buttonStart_Click(object sender, EventArgs e)
-        {
-            for (var i = 0; i < 5; i++)
-                this.ChangeParameterByName(inputTags[i], inputArray[i].Text.Trim());
-            
-            DrawSimulation();
-        }
+			for (var i = 0; i < 5; i++ ) 
+			{
+				inputArray[i] = new TextBox();
+				inputArray[i].Location = new Point(30 + i*100, 30);
+				inputArray[i].Size = new Size(60, 20);
+				inputArray[i].Name = inputTags[i];
+				inputArray[i].Text = this.parameters[inputTags[i]].ToString(CultureInfo.InvariantCulture);
 
-        protected void form_OnMouseDown(object sender, MouseEventArgs e)
-        {
-            Console.WriteLine("MouseDown: ({0}, {1})", e.X, e.Y);
-            arrow[0] = new Point(e.X, e.Y);
+				labelArray[i] = new Label();
+				labelArray[i].Location = new Point(30 + i*100, 10);
+				labelArray[i].Size = new Size(60, 20);
+				labelArray[i].Text = inputLabels[i];
 
-        }
+				this.Controls.Add(inputArray[i]);
+				this.Controls.Add(labelArray[i]);
+			}*/
+			
+			// TODO:Controls (new)
+			int index = 0;
+			foreach(var item in parameters)
+			{
+				Control input = new TextBox();
+				input.Location = new Point(30 + index*100, 30);
+				input.Size = new Size(60, 20);
+				input.Name = item.Key;
+				input.Text = item.Value.ToString(invC);
+				
+				Control label = new Label();
+				label.Location = new Point(30 + index*100, 10);
+				label.Size = new Size(60, 20);
+				label.Text = labels[item.Key];
+				
+				this.Controls.Add(input);
+				this.Controls.Add(label);
+				index++;
+			}
 
-        protected void form_OnMouseUp(object sender, MouseEventArgs e)
-        {
-            Console.WriteLine("MouseUp: ({0}, {1})", e.X, e.Y);
-            arrow[1] = new Point(e.X, e.Y);
+			Button buttonStart = new Button();
+			buttonStart.Location = new Point(30, 70);
+			buttonStart.Size = new Size(60, 25);
+			buttonStart.Text = "Start";
+			buttonStart.Click += buttonStart_Click;
 
-            double arrowX = arrow[1].X - arrow[0].X;
-            double arrowY = arrow[1].Y - arrow[0].Y;
-            double arrowLength = Math.Sqrt(Math.Pow(arrowX, 2) + Math.Pow(arrowY, 2));
-            double arrowAngle = Math.Acos(arrowX / arrowLength);
-            ChangeParameterByName("angle", arrowAngle);
+			fieldResult = new TextBox();
+			fieldResult.Location = new Point(130, 70);
+			fieldResult.Size = new Size(360, 20);
 
-            DrawArrow();
+			this.Controls.Add(buttonStart);
+			this.Controls.Add(fieldResult);
 
-        }
+			this.MouseDown += form_OnMouseDown;
+			this.MouseUp += form_OnMouseUp;
+		}
 
-        private void DrawArrow()
-        {
-            Pen pen = new Pen(Color.FromArgb(255, 0, 0, 255), 4);
-            pen.StartCap = LineCap.ArrowAnchor;
-            pen.EndCap = LineCap.RoundAnchor;
-            formGraphics.DrawLine(pen, arrow[1], arrow[0]);
-        }
+		private void buttonStart_Click(object sender, EventArgs e)
+		{
+			foreach(var item in parameters.ToList())
+			{	
+				var input = this.Controls.Find(item.Key, false)[0];
+				this.UpdateParameterByName(item.Key, Convert.ToDouble(input.Text.Trim(), invC));
+			}
+			
+			DrawSimulation();
+		}
 
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            DrawSimulation();
-        }
+		protected void form_OnMouseDown(object sender, MouseEventArgs e)
+		{
+			Console.WriteLine("MouseDown: ({0}, {1})", e.X, e.Y);
+			arrow[0] = new Point(e.X, e.Y);
 
-        [STAThread]
-        private static void Main(string[] args)
-        {
-            Dictionary<string, double> parameters = new Dictionary<string, double>();
-            parameters.Add("angle", 0);
-            parameters.Add("v0", 0);
-            parameters.Add("mass", 1);
-            parameters.Add("x0", 0);
-            parameters.Add("y0", 0);
+		}
 
-            try
-            {
-                using (var sr = new StreamReader("input.txt"))
-                {
-                    String line;
-                    while ( (line = sr.ReadLine()) != null )
-                    {
-                        var items = line.Split(':');
-                        if (items[1] != null)
-                        {
-                           var key = items[0].Trim();
-                           var value = Convert.ToDouble(items[1].Trim(), CultureInfo.InvariantCulture);
-                           parameters[key] = value;
-                        }
-                    }
-                    sr.Close();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("The file could not be read:");
-                Console.WriteLine(e.Message);
-            }
-            
-            Application.Run(new Program(parameters));
-        }
-    }
+		protected void form_OnMouseUp(object sender, MouseEventArgs e)
+		{
+			Console.WriteLine("MouseUp: ({0}, {1})", e.X, e.Y);
+			arrow[1] = new Point(e.X, e.Y);
+
+			// TODO: pass and count all of this mess separatley
+			double arrowX = arrow[1].X - arrow[0].X;
+			double arrowY = arrow[1].Y - arrow[0].Y;
+			double arrowLength = Math.Sqrt(Math.Pow(arrowX, 2) + Math.Pow(arrowY, 2));
+			double arrowAngle = Math.Acos(arrowX / arrowLength);
+			
+			UpdateParameterByName("angle", arrowAngle);
+
+			DrawArrow();
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			DrawSimulation();
+		}
+
+		private void UpdateParameterByName(String name, double value)
+		{
+			this.parameters[name] = value;
+			var input = this.Controls.Find(name, false)[0];
+			input.Text = value.ToString(invC);
+		}
+
+		private void DrawArrow()
+		{
+			Pen pen = new Pen(Color.FromArgb(255, 0, 0, 255), 3);
+			pen.StartCap = LineCap.ArrowAnchor;
+			pen.EndCap = LineCap.RoundAnchor;
+			formGraphics.DrawLine(pen, arrow[1], arrow[0]);
+		}
+
+		private void DrawSimulation()
+		{
+			Body ball = new Body(parameters["angle"], parameters["v0"],
+				parameters["mass"], parameters["x0"], parameters["y0"]);
+			
+			Simulation flight = new Simulation(ball, formGraphics);
+			var result = flight.Run();
+
+			fieldResult.Text = String.Format("Result: ({1}, {2}) after {0} seconds",
+				result[2], result[0], result[1]);
+		}
+
+		private void ReadParametersFromFile(string filename)
+		{
+			try
+			{
+				using (var sr = new StreamReader(filename))
+				{
+					String line;
+					while ( (line = sr.ReadLine()) != null )
+					{
+						var items = line.Split(':');
+						if (items[1] != null)
+						{
+							var key = items[0].Trim();
+							var value = Convert.ToDouble(items[1].Trim(), invC);
+							this.parameters[key] = value;
+						}
+					}
+					sr.Close();
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("The file could not be read:");
+				Console.WriteLine(e.Message);
+			}
+		}
+
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.Run(new Program("input.txt"));
+			
+			/*
+			Dictionary<string, double> parameters = new Dictionary<string, double>();
+			parameters.Add("angle", 0);
+			parameters.Add("v0", 0);
+			parameters.Add("mass", 1);
+			parameters.Add("x0", 0);
+			parameters.Add("y0", 0);
+			Dictionary<string, string> labels = new Dictionary<string, string>();
+			labels.Add("angle", "Angle:");
+			labels.Add("v0", "Velocity:");
+			labels.Add("mass", "Mass:");
+			labels.Add("x0", "Start X:");
+			labels.Add("y0", "Start Y:");
+			try
+			{
+				using (var sr = new StreamReader("input.txt"))
+				{
+					String line;
+					while ( (line = sr.ReadLine()) != null )
+					{
+						var items = line.Split(':');
+						if (items[1] != null)
+						{
+							var key = items[0].Trim();
+							var value = Convert.ToDouble(items[1].Trim(), CultureInfo.InvariantCulture);
+							parameters[key] = value;
+						}
+					}
+					sr.Close();
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("The file could not be read:");
+				Console.WriteLine(e.Message);
+			}
+			Application.Run(new Program(parameters, labels));
+			*/
+		}
+	}
 }
